676add6f50566daa4ef1b8af023c26ffb9b5999a
rajkumarvoleti <voletirajkumar@gmail.com>
2021-06-12 11:36:32 +0530

    join us form

--- web/src/components/JoinUs.js
+++ web/src/components/JoinUs.js
@@ -6,6 +6,47 @@ import { Formik } from "formik";
 import * as yup from "yup";
 
 class JoinUs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      validation: {
+        role_inValid: false,
+        category_inValid: false,
+      },
+      value: {
+        name: "",
+        email: "",
+        number: "",
+        education: "",
+        role: "",
+        category: "",
+        resume: "",
+      },
+    };
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  // handleChange(event) {
+  //   // this.setState({ value: event.target.value });
+  //   console.log(event.target.value);
+  // }
+
+  // handleSubmit(event) {
+  //   alert("An essay was submitted: " + this.state.value);
+  //   event.preventDefault();
+  // }
+  // .string()
+  // .oneOf(["canEmail", "canText", "canShowUpAtMyHome"])
+  // .required(),
+  // updateForm
+  updateValidation = (str, boole) => {
+    console.log(str);
+    var validation = { ...this.state.validation };
+    const element = str + "_inValid";
+    validation[element] = boole;
+    this.setState({ validation });
+    console.log(this.props);
+  };
   render() {
     const schema = yup.object().shape({
       name: yup.string().required(),
@@ -16,6 +57,8 @@ class JoinUs extends React.Component {
       category: yup.string().required(),
       resume: yup.mixed().required(),
     });
+    const role_inValid = this.state.validation.role_inValid;
+    const category_inValid = this.state.validation.category_inValid;
     return (
       <Formik
         validationSchema={schema}
@@ -25,7 +68,7 @@ class JoinUs extends React.Component {
           email: "",
           number: "",
           education: "",
-          role: "",
+          role: false,
           category: "",
           resume: null,
         }}
@@ -39,7 +82,15 @@ class JoinUs extends React.Component {
           isValid,
           errors,
         }) => (
-          <Form noValidate onSubmit={handleSubmit} className="Join_Us">
+          <Form
+            noValidate
+            onSubmit={(values) => {
+              handleSubmit();
+              alert(JSON.stringify(values, null, 2));
+              console.log(JSON.stringify(values, null, 2));
+            }}
+            className="Join_Us"
+          >
             <div className="join_head">
               <h2>Join Us</h2>
             </div>
@@ -85,8 +136,8 @@ class JoinUs extends React.Component {
                 name="education"
                 type="text"
                 placeholder="Enter your qualifications"
-                value={values.education}
                 onChange={handleChange}
+                value={values.education}
                 isValid={touched.education && !errors.education}
                 isInvalid={!!errors.education}
               />
@@ -94,29 +145,52 @@ class JoinUs extends React.Component {
             <Form.Group>
               <Form.Label>Role</Form.Label>
               <select
+                style={{ display: "block" }}
                 name="role"
-                className="form-control"
+                className={
+                  role_inValid ? "form-control is-valid" : "form-control"
+                }
                 value={values.role}
-                onChange={handleChange}
                 isValid={touched.role && !errors.role}
-                isInvalid={!!errors.role}
-                feedback={errors.role}
+                onChange={(option) => {
+                  handleChange(option);
+                  if (option) this.updateValidation("role", true);
+                  else this.updateValidation("role", true);
+                }}
+                // isInvalid={!!errors.role}
+                // feedback={errors.role}
               >
+                <option hidden selected="selected" value="">
+                  -- select an option --
+                </option>
                 <option value="1">Volunteer</option>
                 <option value="2">Intern</option>
               </select>
+              {errors.color && touched.color && (
+                <div className="input-feedback">{errors.color}</div>
+              )}
             </Form.Group>
             <Form.Group>
               <Form.Label>Intern Category</Form.Label>
               <select
+                disabled={false}
                 name="category"
-                className="form-control"
+                className={
+                  category_inValid ? "form-control is-valid" : "form-control"
+                }
                 value={values.category}
-                onChange={handleChange}
+                onChange={(option) => {
+                  handleChange(option);
+                  if (option) this.updateValidation("category", true);
+                  else this.updateValidation("category", true);
+                }}
                 isValid={touched.category && !errors.category}
                 isInvalid={!!errors.category}
                 feedback={errors.category}
               >
+                <option hidden selected="selected" value>
+                  -- select an option --
+                </option>
                 <option value="1">Social Media</option>
                 <option value="2">Digital Marketing</option>
                 <option value="3">Research</option>
